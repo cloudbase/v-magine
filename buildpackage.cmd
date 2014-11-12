6d690cf534febb40729f06e6fe546a661f600a5e
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-11-12 21:10:44 +0200

    Adds error handling in build script

--- buildpackage.cmd
+++ buildpackage.cmd
@@ -1,5 +1,13 @@
+@echo off
+
 pyinstaller --noconfirm --log-level=WARN --onedir --noconsole --hidden-import=pybootd --hidden-import=socket --icon=resources\app.ico stackinabox.spec 
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 xcopy /S /E /Y www dist\stackinabox\www\
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 xcopy /S /E /Y pxe dist\stackinabox\pxe\
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 xcopy /S /E /Y resources dist\stackinabox\resources\
+if %errorlevel% neq 0 exit /b %errorlevel%
