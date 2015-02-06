24b99ca44135eeed9063a3ba9eb1ca3217909dda
Simona Iuliana Toader <itoader@cloudbasesolutions.com>
2015-02-06 13:52:11 +0200

    Added Show/Hide Password functionality

--- www/scripts/javascript.js
+++ www/scripts/javascript.js
@@ -2,3 +2,10 @@ function showOne(id) {
        $('.hyperv').hide();
          $('#'+id).show();
 }
+
+function showPassword(x){
+   x.type = "text";
+}
+function hidePassword(x){
+  x.type = "password";
+}
