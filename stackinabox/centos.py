cf9cd187f5cda7737dd5ce9b06a23e32b17f6c3a
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2016-02-13 13:39:28 +0200

    Forces CentOS minor version

--- stackinabox/centos.py
+++ stackinabox/centos.py
@@ -1,18 +1,21 @@
-import fastestmirror
 import logging
+import re
 import urllib2
+
+import fastestmirror
 
 from stackinabox import constants
 
 LOG = logging
 
-DEFAULT_CENTOS_MIRROR = "http://mirror.centos.org/centos/7/os/x86_64"
+DEFAULT_CENTOS_RELEASE = "7.2.1511"
+DEFAULT_CENTOS_MIRROR = "http://mirror.centos.org/centos/%s/os/x86_64"
 
 
-def get_repo_mirrors(release="7", arch="x86_64", max_mirrors=8,
-                     sort_by_speed=True):
+def get_repo_mirrors(release=DEFAULT_CENTOS_RELEASE, arch="x86_64",
+                     max_mirrors=8, sort_by_speed=True):
     url_base = "http://mirrorlist.centos.org/?release={0}&arch={1}&repo=os"
-    url = url_base.format(release, arch)
+    url = url_base.format(release.split('.')[0], arch)
 
     mirrors = []
     try:
@@ -22,6 +25,12 @@ def get_repo_mirrors(release="7", arch="x86_64", max_mirrors=8,
     except Exception as ex:
         LOG.exception(ex)
         LOG.error("Failed to get list of CentOS mirrors")
+
+    if len(release.split('.')) > 1:
+        # Use the exact release number, mirrorlist.centos.org accepts only
+        # major release numbers and returns the latest
+        mirrors = [re.sub(r"(.*)/\d+(?:\.\d+)*/(.*)", r"\1/%s/\2" % release, m)
+                   for m in mirrors]
 
     try:
         if sort_by_speed:
@@ -34,7 +43,8 @@ def get_repo_mirrors(release="7", arch="x86_64", max_mirrors=8,
         mirrors = mirrors[:max_mirrors - 1]
 
     # Always add the default mirror
-    if DEFAULT_CENTOS_MIRROR not in mirrors:
-        mirrors.append(DEFAULT_CENTOS_MIRROR)
+    default_mirror = DEFAULT_CENTOS_MIRROR % release
+    if default_mirror not in mirrors:
+        mirrors.append(default_mirror)
 
     return mirrors
