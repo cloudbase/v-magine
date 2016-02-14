a260e9658e0e76a7dbef40ce494e3678d36e1b52
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2016-02-14 19:24:06 +0200

    Adds mkisofs module

---
+++ stackinabox/mkisofs.py
@@ -0,0 +1,24 @@
+import os
+
+from stackinabox import constants
+from stackinabox import utils
+
+
+def create_iso_image(iso_path, content_path, label=constants.PRODUCT_NAME):
+    if os.path.exists(iso_path):
+        os.remove(iso_path)
+
+    normalized_path = content_path.replace("\\", "/")
+    mkisofs = os.path.join(utils.get_bin_dir(), "mkisofs.exe")
+    utils.execute_process([mkisofs,
+                           '-o', iso_path,
+                           '-ldots',
+                           '-allow-lowercase',
+                           '-allow-multidot',
+                           '-l',
+                           '-publisher', constants.PRODUCT_NAME,
+                           '-quiet',
+                           '-J',
+                           '-r',
+                           '-V', label,
+                           normalized_path])
