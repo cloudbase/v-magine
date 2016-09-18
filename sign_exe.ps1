42d38954599538c69b526ca7d57e701f7f8a6799
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2016-09-18 20:03:30 +0300

    Removes signing script

--- sign_exe.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$sha1 = "65c29b06eb665ce202676332e8129ac48d613c61"
-
-$path = "dist\v-magine\v-magine.exe"
-
-& signtool.exe sign /sha1 $sha1 /t http://timestamp.verisign.com/scripts/timstamp.dll /v $path
-if ($LastExitCode) { throw "signtool failed" }
